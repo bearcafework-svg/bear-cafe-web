b19a8aca87f9a9901fcb7412c24522fd0d5b3602
rxbbitz <bearcafe.work@gmail.com>
2026-05-21 20:06:06 +0700

    fix: disable supabase realtime for node 20 koyeb

--- bot/secretChat.js
+++ bot/secretChat.js
@@ -33,12 +33,13 @@ const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY,
   {
-    auth: {
-      persistSession: false,
-      autoRefreshToken: false
-    },
     realtime: {
       enabled: false
+    },
+    auth: {
+      persistSession: false,
+      autoRefreshToken: false,
+      detectSessionInUrl: false
     }
   }
 );
